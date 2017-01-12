b49cd97b2ed91cf91232100ce347739fc4126635
Nicholas Balderas <geist732@gmail.com>
2017-01-12 10:48:24 -0600

    added fontawesome icons

---
+++ src/main/resources/static/JS/masonryConfig.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Nick on 1/12/17.
+ */
+
+$('#grid').masonry({
+    itemSelector: 'grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+});
--- src/main/resources/static/JS/wookmarkIndex.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(new function() {
-    // Call the layout function.
-    $('#tiles li').wookmark({
-        autoResize: true, // This will auto-update the layout when the browser window is resized.
-        container: $('#tiles'), // Optional, used for some extra CSS styling
-        offset: 2, // Optional, the distance between grid items
-        itemWidth: 210 // Optional, the width of a grid item
-    });
-});
